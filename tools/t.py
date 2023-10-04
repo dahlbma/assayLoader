5a534b3691da7a8560c077e30368f0ba76784c4e
Mats Dahlberg <dahlbma@gmail.com>
2023-10-04 12:42:49 +0200

    Update t.py

--- tools/t.py
+++ tools/t.py
@@ -1,9 +1,119 @@
 import pandas as pd
 from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Alignment
+from openpyxl.styles import PatternFill, Alignment, Border, Side, Font
 import openpyxl
+from openpyxl.utils import get_column_letter
 
 outFile = 'colored_cells.xlsx'
+# Define a custom font (Calibri 10pt)
+custom_font = Font(name='Calibri', size=11)
+
+def create_plate_frame(ws, plate_id, top_left_cell, num_columns, num_rows):
+    """
+    Create an Excel sheet with plate_id, letters A-Z, and numbers 1-num_columns.
+
+    Parameters:
+    - plate_id: The text to write in the top_left_cell.
+    - top_left_cell: The top-left cell (e.g., 'A1').
+    - num_columns: The number of columns for numbers (1-num_columns).
+    - num_rows: The number of rows for letters (A-Z).
+    """
+    
+    # Generate letters 'A' to 'Z' below the top_left_cell
+    start_row, start_col = ws[top_left_cell].row, ws[top_left_cell].column
+    
+    # Write the plate_id to the specified cell
+    ws[top_left_cell] = "Plate"
+    cell = ws.cell(row=start_row, column=start_col+1)
+    cell.value = plate_id
+
+    for i in range(num_rows):
+        letter = chr(ord('A') + i)
+        cell = ws.cell(row=start_row + i + 3, column=start_col)
+        cell.value = letter
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+
+    # Generate numbers 1 to num_columns in a row, one column to the right
+    for i in range(num_columns):
+        number = i + 1
+        cell = ws.cell(row=start_row + 2, column=start_col + i + 1)
+        cell.value = number
+        cell.alignment = Alignment(horizontal='center', vertical='center')
+
+
+def create_outer_thick_border(ws, top_left_cell, num_columns, num_rows):
+    """
+    Create a thick border around the outer border of a block of cells in an Excel worksheet.
+
+    Parameters:
+    - ws: The Excel worksheet.
+    - top_left_cell: The top-left cell (e.g., 'A1') of the block.
+    - num_columns: The number of columns in the block.
+    - num_rows: The number of rows in the block.
+    """
+
+    top_left_style = Border(
+        left=Side(style='thick'),
+        right=Side(style=None),
+        top=Side(style='thick'),
+        bottom=Side(style=None)
+    )
+
+    top_right_style = Border(
+        left=Side(style=None),
+        right=Side(style='thick'),
+        top=Side(style='thick'),
+        bottom=Side(style=None)
+    )
+
+    bottom_left_style = Border(
+        left=Side(style='thick'),
+        right=Side(style=None),
+        top=Side(style=None),
+        bottom=Side(style='thick')
+    )
+
+    bottom_right_style = Border(
+        left=Side(style=None),
+        right=Side(style='thick'),
+        top=Side(style=None),
+        bottom=Side(style='thick')
+    )
+
+    # Convert the top-left cell to row and column indices
+    start_row, start_col = ws[top_left_cell].row, ws[top_left_cell].column
+
+    top_l_cell = ws.cell(row=start_row, column=start_col)
+    top_r_cell = ws.cell(row=start_row, column=start_col+num_columns-1)
+    bottom_l_cell = ws.cell(row=start_row+num_rows-1, column=start_col)
+    bottom_r_cell = ws.cell(row=start_row+num_rows-1, column=start_col+num_columns-1)
+
+    # Apply thick border to top row
+    for col in range(start_col, start_col + num_columns):
+        cell = ws.cell(row=start_row, column=col)
+        cell.border = Border(top=Side(style='thick'))
+        top_left_cell = cell
+
+    # Apply thick border to bottom row
+    for col in range(start_col, start_col + num_columns):
+        cell = ws.cell(row=start_row + num_rows - 1, column=col)
+        cell.border = Border(bottom=Side(style='thick'))
+
+    # Apply thick border to left column
+    for row in range(start_row, start_row + num_rows):
+        cell = ws.cell(row=row, column=start_col)
+        cell.border = Border(left=Side(style='thick'))
+
+    # Apply thick border to right column
+    for row in range(start_row, start_row + num_rows):
+        cell = ws.cell(row=row, column=start_col + num_columns - 1)
+        cell.border = Border(right=Side(style='thick'))
+
+    # Apply borders to the corner cells
+    top_l_cell.border = top_left_style
+    top_r_cell.border = top_right_style
+    bottom_l_cell.border = bottom_left_style
+    bottom_r_cell.border = bottom_right_style
 
 def generate_gradient(end_color, start_color, num_steps):
     # Parse the start and end colors into RGB components
@@ -37,8 +147,6 @@ gradient_white_to_blue = generate_gradient(start_color="#0000FF", end_color="#FF
 # Generate the gradient from white to red in 10 steps
 gradient_white_to_red = generate_gradient(start_color="#FF0000", end_color="#FFFFFF", num_steps=10)
 
-print(gradient_white_to_red)
-
 # Create a sample DataFrame with integers between 1 and 100
 data = {'Values': range(1, 101)}
 df = pd.DataFrame(data)
@@ -50,9 +158,27 @@ df.to_excel(writer, sheet_name='Sheet1', index=False)
 writer.close()
 
 
-
 workbook = openpyxl.load_workbook(outFile)
 worksheet = workbook['Sheet1']
+
+worksheet['A1'].font = custom_font
+# Set the zoom factor to 85% (0.85)
+worksheet.sheet_view.zoomScale = 85
+
+# Specify the top-left cell and block dimensions
+start_cell = 'C4'
+num_columns = 24
+num_rows = 16
+
+# Call the function to create the thick border
+create_outer_thick_border(worksheet, start_cell, num_columns, num_rows)
+start_cell = 'C26'
+create_outer_thick_border(worksheet, start_cell, num_columns, num_rows)
+
+
+create_plate_frame(worksheet, '1', 'B1', num_columns, num_rows)
+create_plate_frame(worksheet, '2', 'B23', num_columns, num_rows)
+
 
 
 # Create a blue-to-red gradient fill for cells
@@ -62,24 +188,15 @@ gradient_fill = PatternFill(start_color="0000FF", end_color="FF0000", fill_type=
 for row_idx, row in enumerate(worksheet.iter_rows(min_row=2, max_row=len(df) + 1, min_col=1, max_col=1)):
     cell = row[0]
     value = cell.value
+    cell.font = custom_font
+
     # Apply the gradient fill based on the integer value
     if isinstance(value, int) and 1 <= value <= 100:
         
         percentage = (value - 1) / 99  # Normalize value to range [0, 1]
-        #color = gradient_fill.start_color.rgb
-        #red = int(int(color[2:], 16) + percentage * (int(gradient_fill.end_color.rgb[2:], 16) - int(color[2:], 16)))
-        #blue= int(int(color[:2], 16) + percentage * (int(gradient_fill.end_color.rgb[:2], 16) - int(color[:2], 16)))
-        #print(int(color[2:], 16), percentage, percentage * (int(gradient_fill.end_color.rgb[2:], 16)), (gradient_fill.end_color.rgb[2:], 16), int(color[2:], 16))
-        #print(int(color[2:], 16), int(color[:2], 16), int(gradient_fill.end_color.rgb[:2], 16))
-        #green = 0
-        #fill_color = f"{blue:02X}{green:02X}{red:02X}"
-        
-        #print(value, red, blue, fill_color)
-        #print(fill_color)
         if value < 11:
             cell.fill = PatternFill(start_color=gradient_white_to_red[value], end_color=gradient_white_to_red[value], fill_type="solid")
         elif value > 90:
-            print(value)
             cell.fill = PatternFill(start_color=gradient_white_to_blue[100-value], end_color=gradient_white_to_blue[100-value], fill_type="solid")
 
     # Center-align the cell contents
@@ -88,6 +205,3 @@ for row_idx, row in enumerate(worksheet.iter_rows(min_row=2, max_row=len(df) + 1
 
 #workbook.close()
 workbook.save(outFile)
-
-# Save the workbook
-#writer.close()
