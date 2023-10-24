1ab7598abeb705ea61477d1ea428689e189527df
Mats Dahlberg <dahlbma@gmail.com>
2023-10-24 10:55:16 +0200

    First SP UI commit

--- frontend/singlePointScreen.py
+++ frontend/singlePointScreen.py
@@ -1,4 +1,4 @@
-import re, sys, os, logging
+import re, sys, os, logging, glob
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QFileDialog
 from PyQt5.QtCore import Qt
@@ -19,6 +19,30 @@ class SinglePointScreen(QMainWindow):
         
         self.inputFiles_te.setReadOnly(True)
         saInstruments = dbInterface.getInstruments(self.token)
-
-        print(type(saInstruments))
         self.instrument_cb.addItems(saInstruments)
+        
+        self.dataColumn_eb.editingFinished.connect(self.checkDataColumn)
+
+        self.rawDataDir_btn.clicked.connect(self.selectRawDataDir)
+
+
+    def checkDataColumn(self):
+        # Read a csv file from the inputFiles_te text box and see of the datacolumn in this eb is present
+        pass
+
+    def selectRawDataDir(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly  # Set the option to allow selecting directories only
+
+        directory = QFileDialog.getExistingDirectory(
+            None, "Select Directory", options=options
+        )
+        self.rawDataDir_lab.setText(directory)
+        csv_files = glob.glob(os.path.join(directory, '*.[cC][sS][vV]'))
+
+        sFiles = ''
+        for csv_file in csv_files:
+            sFiles += f'\n{os.path.basename(csv_file)}'
+        self.inputFiles_te.setText(sFiles.lstrip())
+
+    
