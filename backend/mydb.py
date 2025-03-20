380e3e79cc0ab97b1802b648542bdf6b21954f75
Mats Dahlberg <dahlbma@gmail.com>
2025-03-20 09:06:58 +0100

    No more external scarab database

--- backend/mydb.py
+++ backend/mydb.py
@@ -1,5 +1,4 @@
 import MySQLdb 
-from MySQLdb import OperationalError
 import config
 import logging
 
@@ -21,55 +20,35 @@ scarabLogger = setup_logger('scara_logger', 'scarabSqlError.txt')
 class DisconnectSafeCursor(object):
     db = None
     cursor = None
-    conn = None
-    scarabCursor = None
-    scarabConn = None
-    
+    #scarabCursor = None
+
     def __init__(self, db, cursor):
         self.db = db
         self.cursor = cursor
-        self.scarabCursor = db.scarabCur
+        #self.scarabCursor = db.scarabCur
 
     def close(self):
         self.cursor.close()
-        self.scarabCursor.close()
+        #self.scarabCursor.close()
 
     def ping(self, *args, **kwargs):
         ret = ''
-        try:
-            self.db.conn.ping(True)
-            self.db.scarabConn.ping(True)
-        except OperationalError as e:
-            scarabLogger.error(f"OperationalError: {str(e)}")
-            if e.args[0] == 2006:  # MySQL server has gone away
-                scarabLogger.error('Server gone away')
-            ret = 'error'
+        self.cursor.execute(*args, **kwargs)
+
         return ret
-            
+
 
     def execute(self, *args, **kwargs):
         try:
-            if args[0].lstrip().upper().startswith('SELECT'):
-                return self.cursor.execute(*args, **kwargs)
-            else:
-                try:
-                    #self.scarabCursor.execute(*args, **kwargs)
-                    pass
-                except Exception as e:
-                    if '_test' in args[0]:
-                        pass
-                    else:
-                        scarabLogger.error(str(e))
-                        scarabLogger.error(args)
-                return self.cursor.execute(*args, **kwargs)
+            return self.cursor.execute(*args, **kwargs)
         except MySQLdb.OperationalError:
-            ## How can we reconnect in a safe way?
+            logging.error(args[0])
             #self.db.reconnect()
             #self.cursor = self.db.cursor()
+            #return self.cursor.execute(*args, **kwargs)
             pass
-            return self.cursor.execute(*args, **kwargs)
-    
-        
+
+
     def fetchone(self):
         return self.cursor.fetchone()
 
@@ -85,9 +64,6 @@ class DisconnectSafeConnection(object):
     connect_kwargs = None
     conn = None
     cur = None
-    
-    scarabConn = None
-    scarabCur = None
     
     def __init__(self, *args, **kwargs):
         self.connect_args = args
@@ -106,6 +82,7 @@ class DisconnectSafeConnection(object):
         self.conn.autocommit(True)
 
 
+        '''
         self.scarabConn = MySQLdb.connect(
             host=config.scarabDatabase['host'],
             user=config.scarabDatabase['user'],
@@ -113,26 +90,27 @@ class DisconnectSafeConnection(object):
             database=config.scarabDatabase['db'],
             charset='utf8mb4',  # Use utf8mb4 charset
             use_unicode=True    # Use Unicode
-
         )
         self.scarabConn.autocommit(True)
         self.scarabConn.query('SET GLOBAL connect_timeout=28800')
         self.scarabConn.query('SET GLOBAL interactive_timeout=28800')
         self.scarabConn.query('SET GLOBAL wait_timeout=28800')
+        '''
+
 
         
   
     def cursor(self, *args, **kwargs):
         self.cur = self.conn.cursor(*args, **kwargs)
-        self.scarabCur = self.scarabConn.cursor(*args, **kwargs)
+        #self.scarabCur = self.scarabConn.cursor(*args, **kwargs)
         return DisconnectSafeCursor(self, self.cur)
 
     def commit(self):
-        self.scarabConn.commit()
+        #self.scarabConn.commit()
         self.conn.commit()
 
     def rollback(self):
-        self.scarabConn.rollback()
+        #self.scarabConn.rollback()
         self.conn.rollback()
 
 disconnectSafeConnect = DisconnectSafeConnection
